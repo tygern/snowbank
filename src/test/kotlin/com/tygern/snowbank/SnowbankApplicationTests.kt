8868b5907d7c7bf2b88b5035121effa7edad2b77
Tyson Gern <tygern@gmail.com>
2016-03-05 12:36:33 +0000

    Test json response

--- src/test/kotlin/com/tygern/snowbank/SnowbankApplicationTests.kt
+++ src/test/kotlin/com/tygern/snowbank/SnowbankApplicationTests.kt
@@ -1,5 +1,7 @@
 package com.tygern.snowbank
 
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.hasSize
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -9,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -34,7 +36,9 @@ class SnowbankApplicationTests {
         mockMvc!!
                 .perform(get("/flakes"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("[{\"numberOfPoints\":6},{\"numberOfPoints\":5}]"))
+                .andExpect(jsonPath("$", hasSize<Any>(2)))
+                .andExpect(jsonPath("$[0].numberOfPoints", `is`(6)))
+                .andExpect(jsonPath("$[1].numberOfPoints", `is`(5)))
     }
 
 }
