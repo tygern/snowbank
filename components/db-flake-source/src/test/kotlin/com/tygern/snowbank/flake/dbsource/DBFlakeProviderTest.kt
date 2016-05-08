f88f04b96a554267bf41dd44cab34ccc9e885884
Tyson Gern <tygern@gmail.com>
2016-05-08 10:33:04 +0100

    Add first KSpec test

--- components/db-flake-source/src/test/kotlin/com/tygern/snowbank/flake/dbsource/DBFlakeProviderTest.kt
+++ components/db-flake-source/src/test/kotlin/com/tygern/snowbank/flake/dbsource/DBFlakeProviderTest.kt
@@ -1,90 +1,101 @@
 package com.tygern.snowbank.flake.dbsource
 
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.reset
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import com.tygern.snowbank.flake.Flake
+import io.damo.kspec.Spec
 import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertThat
-import org.junit.Before
-import org.junit.Test
 import java.util.*
 
-class DBFlakeProviderTest {
+class DBFlakeProviderTest : Spec({
 
-    lateinit var flakeRepository: FlakeRepository
-    lateinit var flakeProvider: DBFlakeProvider
+    val flakeRepository: FlakeRepository = mock()
+    val flakeProvider = DBFlakeProvider(flakeRepository = flakeRepository)
 
-    @Before
-    fun setUp() {
-        flakeRepository = mock()
-
-        flakeProvider = DBFlakeProvider(flakeRepository = flakeRepository)
+    before {
+        reset(flakeRepository)
     }
 
-    @Test
-    fun testgetList() {
-        val flake = Flake(id = 7, numberOfPoints = 15, pointy = true)
-        val flakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
+    describe("#getList") {
+        test {
+            val flake = Flake(id = 7, numberOfPoints = 15, pointy = true)
+            val flakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
 
-        whenever(flakeRepository.findAll()).thenReturn(Arrays.asList(flakeEntity))
+            whenever(flakeRepository.findAll()).thenReturn(Arrays.asList(flakeEntity))
 
-        var result = flakeProvider.getList()
+            var result = flakeProvider.getList()
 
-        assertThat(result.size, equalTo(1))
-        assertThat(result[0], equalTo(flake))
+            assertThat(result.size, equalTo(1))
+            assertThat(result[0], equalTo(flake))
+        }
     }
 
-    @Test
-    fun testcreate() {
-        val flakeToSave = Flake(id = null, numberOfPoints = 15, pointy = true)
-        val flakeEntity = FlakeEntity(id = null, numberOfPoints = 15, pointy = true)
+    describe("#create") {
+        test {
+            val flakeToSave = Flake(id = null, numberOfPoints = 15, pointy = true)
+            val flakeEntity = FlakeEntity(id = null, numberOfPoints = 15, pointy = true)
 
-        val savedFlake = Flake(id = 7, numberOfPoints = 15, pointy = true)
-        val savedFlakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
+            val savedFlake = Flake(id = 7, numberOfPoints = 15, pointy = true)
+            val savedFlakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
 
-        whenever(flakeRepository.save(flakeEntity)).thenReturn(savedFlakeEntity)
+            whenever(flakeRepository.save(flakeEntity)).thenReturn(savedFlakeEntity)
 
-        var result = flakeProvider.create(flakeToSave)
+            var result = flakeProvider.create(flakeToSave)
 
-        assertThat(result, equalTo(savedFlake))
+            assertThat(result, equalTo(savedFlake))
 
-        verify(flakeRepository).save(flakeEntity)
+            verify(flakeRepository).save(flakeEntity)
+        }
     }
 
-    @Test
-    fun testdelete() {
-        flakeProvider.delete(7)
+    describe("#delete") {
+        test {
+            flakeProvider.delete(7)
 
-        verify(flakeRepository).delete(7)
+            verify(flakeRepository).delete(7)
+        }
     }
 
-    @Test
-    fun testupdate() {
-        val flakeToSave = Flake(id = null, numberOfPoints = 15, pointy = true)
-        val flakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
+    describe("#update") {
+        test {
+            val flakeToSave = Flake(id = null, numberOfPoints = 15, pointy = true)
+            val flakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
 
-        val savedFlake = Flake(id = 7, numberOfPoints = 15, pointy = true)
-        val savedFlakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
+            val savedFlake = Flake(id = 7, numberOfPoints = 15, pointy = true)
+            val savedFlakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
 
-        whenever(flakeRepository.save(flakeEntity)).thenReturn(savedFlakeEntity)
+            whenever(flakeRepository.save(flakeEntity)).thenReturn(savedFlakeEntity)
 
-        var result = flakeProvider.update(7, flakeToSave)
+            var result = flakeProvider.update(7, flakeToSave)
 
-        assertThat(result, equalTo(savedFlake))
+            assertThat(result, equalTo(savedFlake))
 
-        verify(flakeRepository).save(flakeEntity)
+            verify(flakeRepository).save(flakeEntity)
+        }
     }
 
-    @Test
-    fun testget() {
-        val flake = Flake(id = 7, numberOfPoints = 15, pointy = true)
-        val flakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
+    describe("#get") {
+        test {
+            val flake = Flake(id = 7, numberOfPoints = 15, pointy = true)
+            val flakeEntity = FlakeEntity(id = 7, numberOfPoints = 15, pointy = true)
 
-        whenever(flakeRepository.findOne(7)).thenReturn(flakeEntity)
+            whenever(flakeRepository.findOne(7)).thenReturn(flakeEntity)
 
-        var result = flakeProvider.get(7)
+            var result = flakeProvider.get(7)
 
-        assertThat(result, equalTo(flake))
+            assertThat(result, equalTo(flake))
+        }
+
+        test("not found") {
+            whenever(flakeRepository.findOne(7)).thenReturn(null)
+
+            var result = flakeProvider.get(7)
+
+            assertNull(result)
+        }
     }
-}
+})
